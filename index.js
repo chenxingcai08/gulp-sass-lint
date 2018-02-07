ab42eb754eff8c87f5f558117341626d859eaec9
paco <pacochan@foxmail.com>
2018-02-07 17:59:01 +0800

    检测 vue sass code 时过滤掉 sass 代码前后的空行

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ var through = require('through2'),
     PluginError = gutil.PluginError,
     PLUGIN_NAME = 'sass-lint';
 
-var sassLint = function (options) {
+var sassLint = function(options) {
     var userOptions = options || {};
     var configFile = userOptions.configFile;
 
-    var compile = through.obj(function (file, encoding, cb) {
-
+    var compile = through.obj(function(file, encoding, cb) {
         if (file.isNull()) {
             return cb();
         }
@@ -43,7 +42,11 @@ var sassLint = function (options) {
                 var baseLineNumber = fileContent.split(/<style([\s\S]*)lang="scss">/)[0].split(/\r\n|\r|\n/).length;
                 if (matchedVueSassCode && matchedVueSassCode.length) {
                     vueSassCode = matchedVueSassCode[0];
-                    vueSassCode = vueSassCode.replace(/<template>[\s\S]*<style[\s\S]*<\/style>/, '').replace(/<\/style>/gi, '').replace(/<style( scoped)?(="")? lang="scss">/gi, '');
+                    vueSassCode = vueSassCode
+                        .replace(/<template>[\s\S]*<style[\s\S]*<\/style>/, '')
+                        .replace(/<\/style>/gi, '')
+                        .replace(/<style( scoped)?(="")? lang="scss">/gi, '')
+                        .replace(/^[\r\n]+/, '');
                     // 支持顶格缩进和非顶格缩进
                     if (/^\s{4}/.test(vueSassCode)) {
                         vueSassCode = vueSassCode.replace(/^    /gim, '');
@@ -58,11 +61,15 @@ var sassLint = function (options) {
             }
 
             file.sassLint = [
-                lint.lintFileText({
-                    'text': fileBufferContent,
-                    'format': fileType,
-                    'filename': path.relative(process.cwd(), file.path)
-                }, userOptions, configFile)
+                lint.lintFileText(
+                    {
+                        text: fileBufferContent,
+                        format: fileType,
+                        filename: path.relative(process.cwd(), file.path),
+                    },
+                    userOptions,
+                    configFile
+                ),
             ];
         } catch (e) {
             this.emit('error', new PluginError(PLUGIN_NAME, e.message));
@@ -72,10 +79,10 @@ var sassLint = function (options) {
         cb();
     });
     return compile;
-}
+};
 
-sassLint.format = function (writable) {
-    var compile = through.obj(function (file, encoding, cb) {
+sassLint.format = function(writable) {
+    var compile = through.obj(function(file, encoding, cb) {
         if (file.isNull()) {
             return cb();
         }
@@ -95,41 +102,47 @@ sassLint.format = function (writable) {
         cb();
     });
     return compile;
-}
+};
 
-sassLint.failOnError = function () {
+sassLint.failOnError = function() {
     var filesWithErrors = [];
-    var compile = through({ objectMode: true }, function (file, encoding, cb) {
-        if (file.isNull()) {
-            return cb();
+    var compile = through(
+        { objectMode: true },
+        function(file, encoding, cb) {
+            if (file.isNull()) {
+                return cb();
+            }
+
+            if (file.isStream()) {
+                this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+                return cb();
+            }
+
+            if (file.sassLint[0].errorCount > 0) {
+                filesWithErrors.push(file);
+            }
+
+            this.push(file);
+            cb();
+        },
+        function(cb) {
+            var errorMessage;
+
+            if (filesWithErrors.length > 0) {
+                errorMessage = filesWithErrors
+                    .map(function(file) {
+                        return file.sassLint[0].errorCount + ' errors detected in ' + file.relative;
+                    })
+                    .join('\n');
+
+                this.emit('error', new PluginError(PLUGIN_NAME, errorMessage));
+            }
+
+            cb();
         }
-
-        if (file.isStream()) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
-            return cb();
-        }
-
-        if (file.sassLint[0].errorCount > 0) {
-            filesWithErrors.push(file);
-        }
-
-        this.push(file);
-        cb();
-    }, function (cb) {
-        var errorMessage;
-
-        if (filesWithErrors.length > 0) {
-            errorMessage = filesWithErrors.map(function (file) {
-                return file.sassLint[0].errorCount + ' errors detected in ' + file.relative
-            }).join('\n');
-
-            this.emit('error', new PluginError(PLUGIN_NAME, errorMessage));
-        }
-
-        cb();
-    });
+    );
 
     return compile;
-}
+};
 
 module.exports = sassLint;
